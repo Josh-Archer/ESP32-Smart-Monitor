159465d3c1995421ac08ec1da0101d22ea478464
GitHub Action <action@github.com>
2026-08-01 14:45:33 +0000

    Automatic version update to v2.9.1

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,7 @@
 #include "config.h"
 #include "credentials.h"
 
-const char* firmwareVersion = "2.9.0";
+const char* firmwareVersion = "2.9.1";
 
 // WiFi Configuration (from credentials.h)
 const char* ssid = WIFI_SSID;
